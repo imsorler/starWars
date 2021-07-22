1865b0fb352374c557421a753e62f136327eb821
imsorler <imsorler@gmail.com>
2021-07-22 12:33:23 +0300

    add alias

--- src/services/getPeopleData.js
+++ src/services/getPeopleData.js
@@ -4,7 +4,7 @@ import {
     SWAPI_PEOPLE,
     GUID_IMG_EXTENSION,
     URL_IMG_PERSON
-  } from '../constants/api'
+  } from '@constants/api'
 
 const getId = (url, category) => {
   const id = url
