7bfb3a6f401445a1e2071a90f6b9bfa07b166c9a
imsorler <imsorler@gmail.com>
2021-08-18 23:03:21 +0300

    added person info

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 
 import { withErrorApi } from '@hoc/withErrorApi'
 import { getApiResource } from '@utils/network'
@@ -6,17 +6,22 @@ import { getPeopleImage } from '@services/getPeopleData'
 
 import PersonInfo from '@components/PersonPage/PersonInfo'
 import PersonPhoto from '@components/PersonPage/PersonPhoto'
+import PersonLinkBack from '@components/PersonPage/PersonLinkBack'
 
 import { API_PERSON } from '@constants/api'
 
 import PropTypes from 'prop-types'
 
 import styles from './PersonPage.module.css'
+import UILoading from '@ui/UILoading'
+
+const PersonFilms = React.lazy(() => import('@components/PersonPage/PersonFilms'))
 
 const PersonPage = ({ match, setErrorApi }) => {
   const [personInfo, setPersonInfo] = useState(null)
   const [personName, setPersonName] = useState(null)
   const [personPhoto, setPersonPhoto] = useState(null)
+  const [personFilms, setPersonFilms] = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -38,7 +43,7 @@ const PersonPage = ({ match, setErrorApi }) => {
         setPersonName(res.name)
         setPersonPhoto(getPeopleImage(id))
 
-        // res.films
+        res.films.length && setPersonFilms(res.films)
 
         setErrorApi(false)
       } else {
@@ -49,6 +54,7 @@ const PersonPage = ({ match, setErrorApi }) => {
 
   return (
     <>
+    <PersonLinkBack />
       <div className={styles.wrapper}>
         <span className={styles.person__name}>{personName}</span>
         <div className={styles.container}>
@@ -57,6 +63,12 @@ const PersonPage = ({ match, setErrorApi }) => {
             personName={personName}
           />
           {personInfo && <PersonInfo personInfo={personInfo}/> }
+
+          {personFilms && (
+            <Suspense fallback={<UILoading />}>
+              <PersonFilms personFilms={personFilms}/>
+            </Suspense>
+          )}
         </div>
       </div>
     </>
