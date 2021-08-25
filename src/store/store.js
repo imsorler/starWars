20b1e4aba4647bf732b978eea4e59ea8219734b7
imsorler <imsorler@gmail.com>
2021-08-26 01:55:35 +0300

    added redux & localStorage update

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,18 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+import { setLocalStorage } from '@utils/localStorage'
+
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+store.subscribe(() => {
+  setLocalStorage('storage', store.getState().favoriteReducer)
+})
 
 export default store
