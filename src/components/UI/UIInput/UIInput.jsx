363d94ea4745b1a26c38915d67322b700cfd4257
imsorler <imsorler@gmail.com>
2021-08-27 23:50:59 +0300

    added serach component

---
+++ src/components/UI/UIInput/UIInput.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+import icon from './img/cancel.svg'
+import styles from './UIInput.module.css'
+import '../index.css'
+
+const UIInput = ({ value, handleInputChange, placeholder, classes }) => (
+  <div className={cn(styles.wrapper__input, classes)}>
+    <input 
+      type='text'
+      value={value}
+      onChange={(e) => handleInputChange(e.target.value)}
+      placeholder={placeholder}
+      className={styles.input}
+    />
+    <img
+      onClick={() => value && handleInputChange('')}
+      src={icon}
+      className={cn(styles.clear, !value && styles.clear__disabled)}
+      alt='clear'
+    />
+  </div>
+)
+
+
+UIInput.propTypes = {
+  value: PropTypes.string,
+  handleInputChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  classes: PropTypes.string,
+}
+
+export default UIInput
--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -3,6 +3,7 @@ import PeoplePage from '@containers/PeoplePage'
 import NotFoundPage from '@containers/NotFoundPage'
 import PersonPage from '@containers/PersonPage/PersonPage'
 import FavoritesPage from '@containers/FavoritesPage/FavoritesPage'
+import SearchPage from '@containers/SearchPage'
 
 const routesConfig = [
   {
@@ -24,6 +25,11 @@ const routesConfig = [
     path: '/favorites',
     exact: true,
     component: FavoritesPage
+  },
+  {
+    path: '/search',
+    exact: true,
+    component: SearchPage
   },
   {
     path: '/not-found',
